9f3ba65f0e26f5317ab321a6c144051eb9a2d0ed
jhole89 <joel.lutman@gmail.com>
2021-08-05 13:23:58 -0700

    * Rework webpack to avoid separate elm make stage. * Remove elm-stuff from git tracking. * Automate index.html generation. * Organise Elm and JS files into separate dirs.

---
+++ ui/src/js/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+require('/src/js/graph.js');
+var Elm = require('/src/elm/Main.elm').Elm;
+
+var app = Elm.Main.init({
+  node: document.getElementById('orbital-ui')
+});
--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,19 +1,35 @@
-// Webpack uses this to work with directories
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// This is the main configuration object.
-// Here you write different options and tell Webpack what to do
 module.exports = {
-  entry: './src/graph.js',
+
+  entry: './src/js/index.js',
 
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'app.bundle.js'
   },
 
-  // Default mode for Webpack is production.
-  // Depending on mode Webpack will apply different things
-  // on final bundle. For now we don't need production's JavaScript
-  // minifying and other thing so let's set mode to development
-  mode: 'development'
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        loader: "elm-webpack-loader",
+      }
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      filename: 'index.html',
+      template: './src/index.html',
+      title: 'Orbital Data Platform',
+    })
+  ],
+
+  devServer: {
+    inline: true,
+    stats: { colors: true },
+  },
 };
